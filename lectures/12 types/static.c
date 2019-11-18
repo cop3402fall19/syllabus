56b1874778e28b5194e6bc00ebe92f14c61ed2e2
Paul <github@pgazz.com>
2019-11-18 13:59:45 -0500

    added software security lecture materials

--- lectures/12 types/static.c
+++ lectures/12 types/static.c
@@ -2,17 +2,25 @@
 
 int main() {
   printf("STARTED RUNNING\n");
+  
+  /* double temp = (double)2; */
+  /* int y = 1.7 * temp; */
 
-  double temp = (double)2;
-  int y = 1.7 * temp;
+  char *temp2 = "hello, world!";
+  int y2 = 1 + temp2;
 
+  printf("%x\n", *temp2);
+  printf("%x\n", *(temp2 - 30));
 
+  temp2[y2];
+  
+  int a[y2];
 
   /* int y = ((void *)1.7) * (float)((void *)2); */
   
   /* float yf = 1.7 * 2; */
 
-  /* printf("%x\n", y); */
+  /* printf("%x\n", y2); */
   /* printf("%f\n", yf); */
 
   
